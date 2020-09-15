e9fca61d6dde9759eda9814120c2ad42753ca1c6
Harry Coa <rodol_2000@hotmail.com>
2020-09-15 13:00:32 -0500

    agregamos stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FomularioComponent } from './pages/fomulario/fomulario.component';
-
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatStepperModule} from '@angular/material/stepper';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +16,18 @@ import { FomularioComponent } from './pages/fomulario/fomulario.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatStepperModule,
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
